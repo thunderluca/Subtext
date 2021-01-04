d14b45e76f3fa1eb99286bdd2f6c1885ea660aa6
Luca Montanari <luca.montanari@manageddesigns.it>
2021-01-04 10:15:54 +0100

    Added first unit tests on BlobLock class

---
+++ tests/Subtext.Azure.Tests/TestHelper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace Subtext.Azure.Tests
+{
+    internal static class TestHelper
+    {
+        public static void SetProperty<TObject, TValue>(TObject @object, Expression<Func<TObject, TValue>> propertyExpression, TValue value)
+        {
+            if (!(propertyExpression.Body is MemberExpression memberExpression))
+            {
+                throw new ArgumentNullException(nameof(memberExpression));
+            }
+
+            var propertyInfo = memberExpression.Member as PropertyInfo;
+            if (propertyInfo == null)
+            {
+                throw new ArgumentNullException(nameof(propertyInfo));
+            }
+
+            propertyInfo.SetValue(@object, value);
+        }
+    }
+}
