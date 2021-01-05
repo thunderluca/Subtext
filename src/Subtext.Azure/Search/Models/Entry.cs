523dc1b27b505749344bb90dd9d2f4623edbddf5
Luca Montanari <luca.montanari@manageddesigns.it>
2021-01-05 11:45:28 +0100

    Removed Storage implementation and introduced Azure Search ISearchEngineService implementation

---
+++ src/Subtext.Azure/Search/Models/Entry.cs
@@ -0,0 +1,38 @@
+﻿using Azure.Search.Documents.Indexes;
+using System;
+
+namespace Subtext.Azure.Search.Models
+{
+    public class Entry
+    {
+        [SearchableField(IsFilterable = true)]
+        public int BlogId { get; set; }
+
+        [SearchableField(IsFilterable = true)]
+        public string BlogName { get; set; }
+
+        [SearchableField(IsFilterable = true)]
+        public string Body { get; set; }
+
+        [SearchableField(IsFilterable = true)]
+        public int GroupId { get; set; }
+
+        [SearchableField(IsFilterable = true, IsKey = true)]
+        public int Id { get; set; }
+
+        [SearchableField(IsFilterable = true)]
+        public bool IsPublished { get; set; }
+
+        [SearchableField(IsFilterable = true)]
+        public string Name { get; set; }
+
+        [SearchableField(IsFilterable = true, IsSortable = true)]
+        public DateTime PublishDate { get; set; }
+
+        [SearchableField(IsFilterable = true)]
+        public string Tags { get; set; }
+
+        [SearchableField(IsFilterable = true)]
+        public string Title { get; set; }
+    }
+}
